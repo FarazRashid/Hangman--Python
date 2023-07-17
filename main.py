a03e1616a82715011e369fed49e89b9552405ecc
FarazRashid <muhammadfarazrashid@gmail.com>
2023-07-17 20:26:38 +0500

    validating letter

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def get_word():
 def play_Hangman():
     word_to_guess=get_word()
 
-    guessed_letters=['']
+    guessed_letters=[]
 
     while True:
         letter=input("Please enter a letter : ")
+
+# validation
+        for letters in guessed_letters:
+            if letters==letter:
+                print("You have already entered this letter into the program please try again")
+                letter=input("Please enter a letter : ")
+
         guessed_letters.append(letter)
 
-        print("hello ")
         print("You have guessed these letters so far: ")
 
         print(guessed_letters)
