eb0489b1c559db6b77293e68047f0c7d4b678de7
FarazRashid <muhammadfarazrashid@gmail.com>
2023-07-18 19:57:46 +0500

    displaying lives

--- main.py
+++ main.py
@@ -8,22 +8,33 @@ def get_word():
     
 def play_Hangman():
     word_to_guess=get_word()
+    lives=word_to_guess.__len__()
 
     guessed_letters=[]
 
-    while True:
+    while lives>0:
         letter=input("Please enter a letter : ")
 
 # validation
         for letters in guessed_letters:
-            if letters==letter:
+            while letters==letter:
                 print("You have already entered this letter into the program please try again")
                 letter=input("Please enter a letter : ")
 
+        if letter not in word_to_guess:
+            lives-=1
+
         guessed_letters.append(letter)
 
-        print("You have guessed these letters so far: ")
+        print("You have "+ str(lives) + " left and you have guessed these letters: " + str(guessed_letters))
+        print("")
 
-        print(guessed_letters)
+        print("current word: ")
+
+        for letter in word_to_guess:
+                    print("_ ")
+            
+
+
 
 play_Hangman()
