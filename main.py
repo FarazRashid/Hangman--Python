4b494e7353e76214dfecb31b7e4d3422a1dedf59
FarazRashid <muhammadfarazrashid@gmail.com>
2023-07-20 18:11:48 +0500

    fixing up code to print the hangman

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ from words import words
 import random
 
 def get_word():
-    max_number=words.__len__()
-    return(words[random.randint(0,max_number)])
+    return random.choice(words)
     
     
 def play_Hangman():
-
+    
     print("Welcome to hangman!")
     userinput=input("Please press 's' button to start the game: ").lower()
+    
     while userinput=='s':
-        word_to_guess="apple"
-        lives=word_to_guess.__len__()
+        word_to_guess=get_word()
+        lives=len(word_to_guess)
         total_letters=lives
         letters_guessed=0
 
-        display_flags=[False for letters in range(lives)]
+        
 
-        guessed_letters=[]
+        guessed_letters=set()
 
-        while lives>0 and guessed_letters!=total_letters:
+        while lives>0 and guessed_letters!=set(word_to_guess):
             letter=input("Please enter a letter : ")
 
     # validation
@@ -32,29 +32,18 @@ def play_Hangman():
             if letter not in word_to_guess:
                 lives-=1
 
-            guessed_letters.append(letter)
+            guessed_letters.add(letter)
 
-            x=0
 
-            for i in word_to_guess:
-                if letter==i:
-                    display_flags[x]=True
-                    letters_guessed+=1
-                x+=1
 
-            print("You have "+ str(lives) + " left and you have guessed these letters: " + str(guessed_letters))
+            print("You have "+ str(lives) + " left and you have guessed these letters: " + " ".join(guessed_letters))
             print("")
 
-            print("current word: ")
+    
 
-            x=0
+            currentword="".join(letter if letter in guessed_letters else '-' for letter in word_to_guess)
+            print("current word: ", currentword)
 
-            for i in display_flags:
-                if i==False:
-                    print("_",end=" ")
-                elif i==True:
-                    print(word_to_guess[x],end=" ")
-                x+=1
             
             print("")
 
@@ -65,6 +54,7 @@ def play_Hangman():
         if(letters_guessed!=total_letters and lives<=0):
                 
                 print("You have lost the game! You have ran out of lives")
+                print("The word was : " + word_to_guess)
                 break
         
         print("")
